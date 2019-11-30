b262a7f6a0a0fbc5fefb903aa9785c5d559daef9
Weronika Lisay <48102128+weronikalisay7@users.noreply.github.com>
2019-11-30 18:33:31 +0300

    Update Main.java

--- test31/src/main/java/Main.java
+++ test31/src/main/java/Main.java
@@ -5,29 +5,30 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class Main {
     public static void main(String[] args) {
         //Url = "http://google.com";
         System.setProperty("webdriver.chrome.driver","C:/Users/37529/Desktop/учеба/chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
+        ChromeOptions op=new ChromeOptions();
+        op.addArguments("--incognito");
+        op.addArguments("start-maximized");
+        op.addArguments("--disable-search-geolocation-disclosure");
+        WebDriver driver=new ChromeDriver(op);
         driver.navigate().to("https://google.com/");
         WebElement go=driver.findElement(By.name("q"));
         go.sendKeys("banana");
         go.sendKeys(Keys.ENTER);
         WebElement img=driver.findElement(By.linkText("Картинки"));
         img.click();
-        WebElement tools=driver.findElement(By.className("ZXJQ7c"));
+        WebElement tools=driver.findElement(By.linkText("Инструменты"));
         tools.click();
         // by cssSelector  WebElement type=driver.findElement(By.cssSelector(".PKhmud[class='xFo9P']"));
         // by xpath        WebElement type=driver.findElement(By.xpath("//*[contains(@class 'xFo9P']"));
         //type.click();
         driver.quit();
-        WebDriver dr=new ChromeDriver();
-        dr.get("https://selenium.dev/");
-        WebElement home=dr.findElement(By.cssSelector(".hero homepage"));
-        System.out.println(home.getSize());
-        WebElement down=dr.findElement(By.xpath("//*[contains(@class 'download-button-container'"));
-        down.click();
+      
     }
 }
